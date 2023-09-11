5218a0a6ad76e4160a009fb22779c13dc657dd20
luoyoujian <luoyoujian@665e.com>
2023-09-11 09:49:09 +0800

    上传工程

---
+++ Assets/Script/Demo_Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Demo_Test : Observer {
+
+
+    public void OnClickSendNotificationBtn()
+    {
+        ObserverManaager.SendNotification("observer_test", NoteConstant.SEND_TEST, new EventParm("sendNotification", "发送消息"));
+
+    }
+
+    public void OnClickPostNotificationBtn_01()
+    {
+        ObserverManaager.PostNotifcation(NoteConstant.POST_TEST, new EventParm("postNotification", "广播消息01"));
+    }
+
+    public void OnClickPostNotificationBtn_02()
+    {
+        ObserverManaager.PostNotifcation(NoteConstant.SEND_TEST, new EventParm("postNotification", "广播消息02"));
+    }
+
+
+}
---
+++ Assets/Script/Observer_Test.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Observer_Test : Observer
+{
+    private Text send_Text, post_text01,post_text02;
+
+    void Awake()
+    {
+        send_Text = this.transform.Find("SendNotification/Text").GetComponent<Text>();
+        post_text01 = this.transform.Find("PostNotifcation01/Text").GetComponent<Text>();
+        post_text02 = this.transform.Find("PostNotifcation02/Text").GetComponent<Text>();
+        //注册观察者
+        ObserverManaager.RegisterObserver(this);
+        //注册观察者消息通知
+        ObserverManaager.RegisterNotification(this);
+    }
+    void Start()
+    {
+
+    }
+
+    public override string GetName()
+    {
+        return "observer_test";
+    }
+
+    public override string[] GetNote()
+    {
+        return new string[]
+        {
+            NoteConstant.POST_TEST,NoteConstant.SEND_TEST
+        };
+    }
+
+    public override void OnNotification(string note_, EventParm data_)
+    {
+        switch (note_)
+        {
+            case NoteConstant.POST_TEST:
+                {
+                    if (data_.IsExist("postNotification"))
+                    {
+                        post_text01.text = (string)data_["postNotification"];
+                    }
+                    
+
+                    break;
+                }
+            case NoteConstant.SEND_TEST:
+                {
+                    if (data_.IsExist("sendNotification"))
+                    {
+                        send_Text.text = (string)data_["sendNotification"];
+                    }
+                    if (data_.IsExist("postNotification"))
+                    {
+                        post_text02.text = (string)data_["postNotification"];
+                    }
+
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+
+    void OnDestroy()
+    {
+        //撤消观察者
+        ObserverManaager.UnregiserObserver(this);
+        //撤消观察者消息通知
+        ObserverManaager.UnregiserNotification(this);
+    }
+
+}
